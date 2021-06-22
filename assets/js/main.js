9966122861c9b4e7dfcac36c889cbc12197e2f50
Murilo Vitória da Silva <40902924+MuriloVS@users.noreply.github.com>
2021-06-22 20:50:36 -0300

    inicio

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function header() {
+  document.write(
+    '<div class="navbar"><a href="index.html">Home</a><a href="integrantes.html">Integrantes</a><a href="discografia.html">Discografia</a><a href="contato.html">Contato</a></div>'
+  );
+}
+
+function footer() {
+  document.write(
+    '<div class="footer"><a target="_blank" href="https://github.com/MuriloVS">Murilo Vitória da Silva\'s Github</a></div>'
+  );
+}
