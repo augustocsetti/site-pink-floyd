809cba6709e0d6288f8fe8ba45fd664eab51728f
MuriloVS <40902924+MuriloVS@users.noreply.github.com>
2021-06-22 22:26:46 -0300

    scripts e css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 function header() {
   document.write(
-    '<div class="navbar"><a href="index.html">Home</a><a href="integrantes.html">Integrantes</a><a href="discografia.html">Discografia</a><a href="contato.html">Contato</a></div>'
+    '<header><div class="navbar"><a href="index.html">Home</a><a href="integrantes.html">Integrantes</a><a href="discografia.html">Discografia</a><a href="contato.html">Contato</a></div></header>'
   );
+  console.log('oi');
 }
 
 function footer() {
   document.write(
-    '<div class="footer"><a target="_blank" href="https://github.com/MuriloVS">Murilo Vitória da Silva\'s Github</a></div>'
+    '<footer><div class="footer"><a target="_blank" href="https://github.com/MuriloVS">Murilo Vitória da Silva\'s Github</a></div></footer>'
   );
 }
