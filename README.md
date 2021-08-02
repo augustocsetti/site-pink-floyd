b2bbc64a7d0fe27760549e948eedcd12145a0ec8
augusto <augus@DESKTOP-R1UCUSG>
2021-08-02 18:42:39 -0300

    commit test main2.0

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Este repo é para um trabalho avaliado da disciplina optativa de Sistemas
 para Internet II.
 
 O objetivo é criar um site usando somente com HTML, CSS e Javascript.
+
+FAZER:
+
+- Mudar fonte do título
+- HTML Semântico
+- Site responsivo
+
+- Ajustar posição do box de texto
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,48 @@
-import { atom, more, piper, saucerful, ummagumma } from './discografia.js';
-import { david, nick, richard, roger, syd } from './integrantes.js';
+import { atom, more, piper, saucerful, ummagumma } from "./discografia.js";
+import { david, nick, richard, roger, syd } from "./integrantes.js";
 
-const integrantesContent = document.getElementById('integrante-conteudo');
-if (integrantesContent) {
-  integrantesContent.addEventListener('click', (evento) => {
-    const elemento = evento.target.id;
+const integrantesContent = document.getElementById("integrante-conteudo");
+const discografiaContent = document.getElementById("discografia-conteudo");
 
-    if (elemento === 'syd') {
-      integrantesContent.innerHTML = syd;
-    } else if (elemento === 'nick') {
-      integrantesContent.innerHTML = nick;
-    } else if (elemento === 'roger') {
-      integrantesContent.innerHTML = roger;
-    } else if (elemento === 'richard') {
-      integrantesContent.innerHTML = richard;
-    } else if (elemento === 'david') {
-      integrantesContent.innerHTML = david;
-    }
-  });
-}
+document.addEventListener("click", (evento) => {
+  const elemento = evento.target;
+  // integrantes
+  if (elemento.id === "syd") {
+    integrantesContent.innerHTML = syd;
+  } else if (elemento.id === "nick") {
+    integrantesContent.innerHTML = nick;
+  } else if (elemento.id === "roger") {
+    integrantesContent.innerHTML = roger;
+  } else if (elemento.id === "richard") {
+    integrantesContent.innerHTML = richard;
+  } else if (elemento.id === "david") {
+    integrantesContent.innerHTML = david;
+  }
+  // discografia
+  else if (elemento.id === "piper") {
+    discografiaContent.innerHTML = piper;
+  } else if (elemento.id === "saucerful") {
+    discografiaContent.innerHTML = saucerful;
+  } else if (elemento.id === "more") {
+    discografiaContent.innerHTML = more;
+  } else if (elemento.id === "ummagumma") {
+    discografiaContent.innerHTML = ummagumma;
+  } else if (elemento.id === "atom") {
+    discografiaContent.innerHTML = atom;
+  }
+});
 
-const discografiaContent = document.getElementById('discografia-conteudo');
-if (discografiaContent) {
-  discografiaContent.addEventListener('click', (evento) => {
-    const elemento = evento.target.id;
-
-    if (elemento === 'piper') {
-      discografiaContent.innerHTML = piper;
-    } else if (elemento === 'saucerful') {
-      discografiaContent.innerHTML = saucerful;
-    } else if (elemento === 'more') {
-      discografiaContent.innerHTML = more;
-    } else if (elemento === 'ummagumma') {
-      discografiaContent.innerHTML = ummagumma;
-    } else if (elemento === 'atom') {
-      discografiaContent.innerHTML = atom;
-    }
-  });
-}
-
-const headerContent = document.getElementById('header');
+const headerContent = document.getElementById("header");
 headerContent.innerHTML = `<div class="navbar">
+      <h1 class = "title-page">O Site do Murilo</h1>
       <a href="index.html">Home</a>
       <a href="integrantes.html">Integrantes</a>
       <a href="discografia.html">Discografia</a>
       <a href="contato.html">Contato</a>
     </div>`;
 
-const footerContent = document.getElementById('footer');
+const footerContent = document.getElementById("footer");
 footerContent.innerHTML = `<div class="footer">
-      <a target="_blank" href="https://github.com/augustocsetti">
-      Augusto Cardoso Setti's Github
-      </a>      
-      <a target="_blank" href="https://github.com/MuriloVS">
-        Murilo Vitória da Silva's Github
-      </a>
+      <a target="_blank" href="https://github.com/augustocsetti">Augusto Cardoso Setti's Github</a>      
+      <a target="_blank" href="https://github.com/MuriloVS">Murilo Vitória da Silva's Github</a>
     </div>`;
